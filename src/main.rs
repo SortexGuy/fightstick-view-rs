0c4b6d3a267944e416a7f2ed9689a6efa57264c9
Rubén A <sortexjuegos@gmail.com>
2024-01-28 17:25:40 -0400

    Modularization

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,19 @@ use std::option::Option::*;
 
 #[derive(Debug, Clone, PartialEq)]
 struct FSState {
-    axis: FSAxis,
+    axis: Vector2,
     buttons: Vec<Button>,
 }
 
-#[derive(Debug, Clone, PartialEq)]
-struct FSAxis {
-    x: i8,
-    y: i8,
+enum FSButton {
+    West = 0,
+    South = 1,
+    North = 2,
+    East = 3,
+    RightTrigger = 4,
+    RightTrigger2 = 5,
+    LeftTrigger = 6,
+    LeftTrigger2 = 7,
 }
 
 fn main() {
@@ -25,7 +30,7 @@ fn main() {
     rl.set_target_fps(60);
 
     let mut fs_state_history: Vec<FSState> = vec![FSState {
-        axis: FSAxis { x: 0, y: 0 },
+        axis: Vector2 { x: 0.0, y: 0.0 },
         buttons: vec![],
     }];
 
@@ -48,56 +53,18 @@ fn main() {
                 Event {
                     event: EventType::ButtonChanged(button, val, _),
                     ..
-                } => {
-                    // Controlling the axis with the dpad
-                    if Button::is_dpad(button) {
-                        if [Button::DPadLeft, Button::DPadRight].contains(&button) {
-                            if val > 0.25 {
-                                axis.x = if button == Button::DPadLeft { -1 } else { 1 };
-                            } else {
-                                axis.x = 0;
-                            };
-                        } else if val > 0.25 {
-                            axis.y = if button == Button::DPadDown { -1 } else { 1 };
-                        } else {
-                            axis.y = 0;
-                        };
-                    } else if val > 0.25 && !buttons.contains(&button) {
-                        buttons.push(button);
-                    } else if buttons.contains(&button) {
-                        buttons.retain(|b| *b != button);
-                    }
-                    // println!("Button {:?} state changed to {} by {}", button, val, id);
-                }
+                } => handle_buttons(buttons, axis, button, val),
                 Event {
                     event: EventType::AxisChanged(ax, val, _),
                     ..
-                } => {
-                    if [Axis::DPadX, Axis::LeftStickX].contains(&ax) {
-                        axis.x = (val.signum() * if val.abs() > 0.25 { 1. } else { 0. }) as i8;
-                    } else if [Axis::DPadY, Axis::LeftStickY].contains(&ax) {
-                        axis.y = (val.signum() * if val.abs() > 0.25 { 1. } else { 0. }) as i8;
-                    } else if [Axis::LeftZ, Axis::RightZ].contains(&ax) {
-                        if val > 0.25 {
-                            if ax == Axis::LeftZ {
-                                buttons.push(Button::LeftTrigger2);
-                            } else if ax == Axis::RightZ {
-                                buttons.push(Button::RightTrigger2);
-                            }
-                        } else if buttons.contains(&Button::LeftTrigger2) {
-                            buttons.retain(|b| *b != Button::LeftTrigger2);
-                        } else if buttons.contains(&Button::RightTrigger2) {
-                            buttons.retain(|b| *b != Button::RightTrigger2);
-                        }
-                    }
-                }
+                } => handle_axis(buttons, axis, ax, val),
                 Event {
                     event: EventType::Disconnected,
                     ..
                 } => {
                     fs_state_history.clear();
                     fs_state = FSState {
-                        axis: FSAxis { x: 0, y: 0 },
+                        axis: Vector2 { x: 0., y: 0. },
                         buttons: vec![],
                     };
                 }
@@ -110,47 +77,169 @@ fn main() {
         if fs_state_history.len() >= max_history {
             fs_state_history.remove(0);
         }
-        {
-            //* Raylib Drawing
-            let mut d = rl.begin_drawing(&thread);
-            d.clear_background(Color::new(0, 0, 0, 0));
 
-            // TODO: Draw Stick
+        //* Raylib Drawing
+        let mut d = rl.begin_drawing(&thread);
+        draw_fightstick(&mut d, &fs_state_history);
+        drop(d);
+    }
+}
 
-            // Draw buttons
-            let radius = 56.;
-            let start_btn_pos = (6 * radius as i32, (1.2 * radius) as i32);
-            let sep = (radius as i32) * 2 + 8; // Separation
-            for i in 0..8 {
-                let center_x = start_btn_pos.0 + sep * (i / 2 + 1);
-                let center_y =
-                    start_btn_pos.1 + sep * (i % 2 + 1) + if i < 2 { radius as i32 } else { 0 };
-                let color: Color = {
-                    let btns = fs_state_history.last().cloned().unwrap().buttons;
-                    if btns.contains(&Button::North) && i == 0
-                        || btns.contains(&Button::South) && i == 1
-                        || btns.contains(&Button::West) && i == 2
-                        || btns.contains(&Button::East) && i == 3
-                        || btns.contains(&Button::RightTrigger) && i == 4
-                        || btns.contains(&Button::RightTrigger2) && i == 5
-                        || btns.contains(&Button::LeftTrigger) && i == 6
-                        || btns.contains(&Button::LeftTrigger2) && i == 7
-                    {
-                        Color::RED
-                    } else {
-                        Color::DARKBLUE
-                    }
+fn handle_buttons(buttons: &mut Vec<Button>, axis: &mut Vector2, button: Button, val: f32) {
+    // Controlling the axis with the dpad
+    if Button::is_dpad(button) {
+        if [Button::DPadLeft, Button::DPadRight].contains(&button) {
+            if val > 0.25 {
+                axis.x = if button == Button::DPadLeft {
+                    -1.0
+                } else {
+                    1.0
                 };
-                d.draw_circle(center_x, center_y, radius, color);
-            }
+            } else {
+                axis.x = 0.0;
+            };
+        } else if val > 0.25 {
+            axis.y = if button == Button::DPadDown {
+                -1.0
+            } else {
+                1.0
+            };
+        } else {
+            axis.y = 0.0;
+        };
+    } else if val > 0.25 && !buttons.contains(&button) {
+        buttons.push(button);
+    } else if buttons.contains(&button) {
+        buttons.retain(|b| *b != button);
+    }
+    // println!("Button {:?} state changed to {} by {}", button, val, id);
+}
 
-            d.draw_text(
-                &format!("{:?}", fs_state_history.last().unwrap()),
-                20,
-                540 - 14,
-                12,
-                Color::WHITE,
-            );
+fn handle_axis(buttons: &mut Vec<Button>, axis: &mut Vector2, ax: Axis, val: f32) {
+    if [Axis::DPadX, Axis::LeftStickX].contains(&ax) {
+        axis.x = val.signum() * if val.abs() > 0.25 { 1. } else { 0. };
+    } else if [Axis::DPadY, Axis::LeftStickY].contains(&ax) {
+        axis.y = val.signum() * if val.abs() > 0.25 { 1. } else { 0. };
+    } else if [Axis::LeftZ, Axis::RightZ].contains(&ax) {
+        if val > 0.25 {
+            if ax == Axis::LeftZ {
+                buttons.push(Button::LeftTrigger2);
+            } else if ax == Axis::RightZ {
+                buttons.push(Button::RightTrigger2);
+            }
+        } else if buttons.contains(&Button::LeftTrigger2) {
+            buttons.retain(|b| *b != Button::LeftTrigger2);
+        } else if buttons.contains(&Button::RightTrigger2) {
+            buttons.retain(|b| *b != Button::RightTrigger2);
         }
     }
+}
+
+fn draw_fightstick(d: &mut RaylibDrawHandle, fs_state_history: &Vec<FSState>) {
+    d.clear_background(Color::new(0, 0, 0, 0));
+    let radius = 56.;
+    let sep = (radius as i32) * 2 + 8; // Separation
+    let FSState {
+        axis: stick,
+        buttons: btns,
+    } = fs_state_history.last().cloned().unwrap();
+
+    {
+        //* Draw Stick
+        // let i = max_history;
+        // stick_inputs.iter().rev().collect();
+        // for i in (0..max_history).rev()
+
+        let sep = (sep as f32 * 0.75) as i32;
+        // let stick = curr_state.axis;
+        let center_pos = Vector2 {
+            x: 3.5 * radius,
+            y: 5.2 * radius,
+        };
+        let stick_inputs = fs_state_history
+            .iter()
+            .map(|s| Vector2 {
+                x: center_pos.x + (s.axis.x * sep as f32),
+                y: center_pos.y + (-s.axis.y * sep as f32),
+            })
+            .rev()
+            .collect::<Vec<Vector2>>();
+
+        {
+            let mut i: usize = 0;
+            while i < stick_inputs.len() - 1 {
+                d.draw_line_ex(stick_inputs[i], stick_inputs[i + 1], 4.0, Color::GOLD);
+                i += 1;
+            }
+        }
+        // d.draw_line_strip(stick_inputs.as_slice(), Color::GOLD);
+        d.draw_rectangle_rounded_lines(
+            Rectangle {
+                x: center_pos.x - (sep) as f32,
+                y: center_pos.y - (sep) as f32,
+                width: (sep * 2) as f32,
+                height: (sep * 2) as f32,
+            },
+            0.25,
+            16,
+            2,
+            Color::LIGHTGRAY,
+        );
+
+        let color = if stick.x != 0.0 || stick.y != 0.0 {
+            Color::RED
+        } else {
+            Color::LIGHTGRAY
+        };
+        d.draw_circle(
+            center_pos.x as i32 + (sep * stick.x as i32),
+            center_pos.y as i32 + (sep * -stick.y as i32),
+            radius,
+            color,
+        );
+    }
+
+    {
+        //* Draw button Layout
+        let start_btn_pos = (6 * radius as i32, (1.2 * radius) as i32);
+        for i in 0..8 {
+            let center_x = start_btn_pos.0 + sep * (i / 2 + 1);
+            let center_y =
+                start_btn_pos.1 + sep * (i % 2 + 1) + if i < 2 { radius as i32 } else { 0 };
+            let color: Color = {
+                // let btns = curr_state.buttons;
+                if btns.contains(&Button::West) && i == FSButton::West as i32
+                    || btns.contains(&Button::South) && i == FSButton::South as i32
+                    || btns.contains(&Button::North) && i == FSButton::North as i32
+                    || btns.contains(&Button::East) && i == FSButton::East as i32
+                    || btns.contains(&Button::RightTrigger) && i == FSButton::RightTrigger as i32
+                    || btns.contains(&Button::RightTrigger2) && i == FSButton::RightTrigger2 as i32
+                    || btns.contains(&Button::LeftTrigger) && i == FSButton::LeftTrigger as i32
+                    || btns.contains(&Button::LeftTrigger2) && i == FSButton::LeftTrigger2 as i32
+                {
+                    Color::RED
+                } else {
+                    Color::DARKBLUE
+                }
+            };
+            d.draw_circle(center_x, center_y, radius, color);
+        }
+    }
+
+    d.draw_text(
+        &format!("{:?}", fs_state_history.last().unwrap()),
+        20,
+        540 - 14,
+        12,
+        Color::WHITE,
+    );
+}
+
+// (1 - t) * v0 + t * v1
+fn lerp_color(c_start: Color, c_end: Color, amount: f32) -> Color {
+    let r = ((1.0 - amount) * c_start.r as f32 + amount * c_end.r as f32) as u8;
+    let g = ((1.0 - amount) * c_start.g as f32 + amount * c_end.g as f32) as u8;
+    let b = ((1.0 - amount) * c_start.b as f32 + amount * c_end.b as f32) as u8;
+    let a = ((1.0 - amount) * c_start.a as f32 + amount * c_end.a as f32) as u8;
+    Color { r, g, b, a }
 }
